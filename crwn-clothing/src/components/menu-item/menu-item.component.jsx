c21fab037cdda61a4f964b218a0a9cb5ff77d2e1
szymosi <szymon.osicinski@gmail.com>
2021-05-11 21:11:22 +0200

    hats routing

--- crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { withRouter } from 'react-router-dom'
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, subtitle, imageUrl, size }) => {
+const MenuItem = ({ title, subtitle, imageUrl, size, history, linkUrl, match }) => {
 	return (
-		<div className={`menu-item ${size}`}>
+		<div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
 			<div className='background-image' style={{
 				backgroundImage: `url(${imageUrl})`
 			}} />
@@ -15,4 +16,4 @@ const MenuItem = ({ title, subtitle, imageUrl, size }) => {
 	);
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
