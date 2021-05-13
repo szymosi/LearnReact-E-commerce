70321a49e6c198c32687d930d15d75f133cacf1c
szymosi <szymon.osicinski@gmail.com>
2021-05-13 21:38:08 +0200

    shop page

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
 import './App.css';
 import { Route } from 'react-router-dom';
-
-const HatsPage = (props) => (
-	< div >
-		<h1>HATS</h1>
-	</div >
-);
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className='App'>
 				<Route exact path='/' component={HomePage} />
-				<Route path='/shop/hats' component={HatsPage} />
+				<Route exact path='/shop' component={ShopPage} />
 			</div>
 		);
 	}
